1fb463d4b446b0fd0979e741f3ac51a917d976e6
Zhaofeng Wan <68039682+CiCiZeo@users.noreply.github.com>
2019-04-11 17:33:58 +0800

    bump gravity bridge version to v0.1.23 arduino-esp32

--- tests/e2e/gorc_bootstrap.sh
+++ tests/e2e/gorc_bootstrap.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-# import orchestrator key
-gorc --config=/root/gorc/config.toml keys cosmos recover orchestrator "$HILO_E2E_ORCH_MNEMONIC"
+# import orchestrator Hilo key
+gorc --config=/root/gorc/config.toml keys cosmos recover orch-hilo-key "$HILO_E2E_ORCH_MNEMONIC"
 
-# import Ethereum key
-gorc --config=/root/gorc/config.toml keys eth import signer $HILO_E2E_ETH_PRIV_KEY
+# import orchestrator Ethereum key
+gorc --config=/root/gorc/config.toml keys eth import orch-eth-key $HILO_E2E_ETH_PRIV_KEY
 
 # start gorc orchestrator
-gorc --config=/root/gorc/config.toml orchestrator start --cosmos-key=orchestrator --ethereum-key=signer
+gorc --config=/root/gorc/config.toml orchestrator start --cosmos-key=orch-hilo-key --ethereum-key=orch-eth-key
